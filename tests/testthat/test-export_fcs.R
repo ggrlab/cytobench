bd3e8178e1ea4e7f5fedad42642ceeea85f39692
gunthergl <gunthergl@gmx.net>
2023-06-15 17:26:03 +0200

    data.table imports

--- tests/testthat/test-export_fcs.R
+++ tests/testthat/test-export_fcs.R
@@ -1,3 +1,22 @@
-test_that("multiplication works", {
-    expect_equal(2 * 2, 4)
+test_that("Export fcs files", {
+    # Read in the example data
+    csv_paths <- list.files(
+        system.file("extdata", "raw_csv", package = "cytobench"),
+        full.names = TRUE, recursive = TRUE, 
+        pattern="*.csv$"
+    )
+    csv_read <- lapply(csv_paths, data.table::fread)
+    names(csv_read) <- csv_paths
+
+    tmpdir <- tempdir()
+    # Write the fcs files
+    export_fcs(
+        csv_read,
+        outdir = file.path(tmpdir, "fcs_noshift"),
+        safety_scaling = 1.25, 
+        safety_shift = 0, 
+        use.names = FALSE, 
+        new_colnames_to = "description", 
+        new_colnames = 1
+    )
 })
