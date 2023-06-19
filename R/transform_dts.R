c3a514b08c87831366fe376a7181f0ab4068228b
gunthergl <gunthergl@gmx.net>
2023-06-19 09:51:39 +0200

    0.2.0: transform_dts

--- R/transform_dts.R
+++ R/transform_dts.R
@@ -1,26 +1,26 @@
 #' @export
-transform <- function(matrix_list,
-                      scalings_names_unified = c(
-                          "FS INT" = -14,
-                          "FS TOF" = -14,
-                          "SS INT" = -14,
-                          "FL1 CD45RA FITC" = 50e3,
-                          "FL2 CCR7 PE" = 50e3,
-                          "FL3 CD28 ECD" = 50e3,
-                          "FL4 PD1 PC5.5" = 50e3,
-                          "FL5 CD27 PC7" = 50e3,
-                          "FL6 CD4 APC" = 50e3,
-                          "FL7 CD8 AF700" = 50e3,
-                          "FL8 CD3 AA750" = 50e3,
-                          "FL9 CD57 PB" = 50e3,
-                          "FL10 CD45 KrO" = 50e3,
-                          "TIME" = -14
-                      ),
-                      notransform_special_number = -14,
-                      feature_unified_dict = feature_unified_dict_default(),
-                      transform_fun = function(x_values, scaling_factor) {
-                          asinh(x_values / scaling_factor)
-                      }, inplace = TRUE) {
+transform_dts <- function(datatable_list,
+                          scalings_names_unified = c(
+                              "FS INT" = -14,
+                              "FS TOF" = -14,
+                              "SS INT" = -14,
+                              "FL1 CD45RA FITC" = 50e3,
+                              "FL2 CCR7 PE" = 50e3,
+                              "FL3 CD28 ECD" = 50e3,
+                              "FL4 PD1 PC5.5" = 50e3,
+                              "FL5 CD27 PC7" = 50e3,
+                              "FL6 CD4 APC" = 50e3,
+                              "FL7 CD8 AF700" = 50e3,
+                              "FL8 CD3 AA750" = 50e3,
+                              "FL9 CD57 PB" = 50e3,
+                              "FL10 CD45 KrO" = 50e3,
+                              "TIME" = -14
+                          ),
+                          notransform_special_number = -14,
+                          feature_unified_dict = feature_unified_dict_default(),
+                          transform_fun = function(x_values, scaling_factor) {
+                              asinh(x_values / scaling_factor)
+                          }, inplace = TRUE) {
     if (!inplace) {
         # That here effectively generates a new datatable
         # If the matrix _IS_ already a datatable, effectively a copy is created.
