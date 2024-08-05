09ba1b44b84cbe12541e9cd2f88ee8ea887d6914
gunthergl_r1 <gunthergl@gmx.net>
2024-08-05 18:04:27 +0200

    update cmv_helper_compensation with expected_max_proportion_value

--- R/cmv_helper_compensation.R
+++ R/cmv_helper_compensation.R
@@ -12,19 +12,25 @@
 #' Compensation TUBES from Beckman Coulter. "auto_singlestain" calculates the spillover matrix and
 #' autofluorescence vector  based on the single stain controls from the CMV data.
 #' "manual" is the default option, which uses the manually curated spillover matrix and autofluorescence vector.
+#' @param expected_max_proportion_value
+#' The expected maximum value for the spillover matrix and autofluorescence vector. Default is 1 which corresponds to 100%. However, technically nothing speaks against spillover values above 100%.
+#' Within Kaluza, the spillover values can be a maximum of +/- 500% (+/-5). Thus the default value of 5. This is a safety measure to avoid using the already calculated absolute autofluorescence values.
 #' @export
 cmv_helper_compensation <- function(ff_unapplied_compensations,
-                                    compensation_type = c("manual", "auto", "auto_singlestain")) {
+                                    compensation_type = c("manual", "auto", "auto_singlestain"), expected_max_proportion_value = 5) {
     params_spillover_autofluorescence <- read.FCS_custom_spillover(
         ff_unapplied_compensations,
         custom_spillover_keyword = compensation_type[1]
     )
     if (any(
-        sapply(params_spillover_autofluorescence, function(x) any(x > 1))
+        sapply(params_spillover_autofluorescence, function(x) any(x > expected_max_proportion_value))
     )) {
         stop(
-            "Spillover matrix or autofluorescence vector contains values > 1.",
-            "This would mean that these are not percentages."
+            paste0(
+                "Spillover matrix or autofluorescence vector contains values > ",
+                expected_max_proportion_value,
+                ". This would mean that these are most probably not percentages."
+            )
         )
     }
     ff_compensated <- cytoKal::compensation_autofluorescence_kaluza(
@@ -42,6 +48,14 @@ cmv_helper_compensation <- function(ff_unapplied_compensations,
 }
 
 
+#' Read custom spillover matrix and autofluorescence vector from FCS file
+#'
+#' Be careful when using it. This is primarily a helper function for the cmv_helper_compensation function and the related data.
+#' @param fcs
+#' Either a path to an FCS file or a flowFrame object
+#' @param custom_spillover_keyword
+#' The keyword to use for the custom spillover matrix and autofluorescence vector. Default is "spillover.manual".
+#' @export
 read.FCS_custom_spillover <- function(fcs, custom_spillover_keyword = "spillover.manual") {
     if (is.character(fcs)) {
         # Reading the stored spillover matrices:
