6f3f27a119b8f8a6447ff0c9123c28b51dbcdba2
gunthergl_r1 <gunthergl@gmx.net>
2024-09-02 16:19:36 +0200

    added comments

--- R/extract_singlestain_mfi.R
+++ R/extract_singlestain_mfi.R
@@ -1,4 +1,3 @@
-
 extract_singlestain_mfi <- function(fcs_dir = "data-raw/s001",
                                     regex_singlestain = "-(CD3-.*)|(none)\\.fcs",
                                     transform = function(x) {
@@ -6,19 +5,28 @@ extract_singlestain_mfi <- function(fcs_dir = "data-raw/s001",
                                     },
                                     gating_set_file = NULL,
                                     gate_extract = NULL) {
+    # List all files in the specified directory that match the given regex pattern
     dir_files <- list.files(fcs_dir, full.names = TRUE, pattern = regex_singlestain)
+    
+    # Load the FCS files into a list of cytosets
     loaded_fcs <- sapply(dir_files, flowWorkspace::load_cytoset_from_fcs, simplify = FALSE)
+    
+    # Check if a gating set file is provided
     if (!all(is.null(gating_set_file))) {
+        # Load the gating set from file if it's a character string, otherwise use the provided object
         if ("character" %in% class(gating_set_file)) {
             gatingset <- flowWorkspace::load_gs(gating_set_file)
         } else {
             gatingset <- gating_set_file
         }
-        # Then gate the fcs files
+        
+        # Apply the gating set to each loaded FCS file
         loaded_fcs <- lapply(loaded_fcs, function(ff_x) {
             suppressMessages(
                 ff_x_gated <- flowWorkspace::gh_apply_to_cs(gatingset, ff_x, compensation_source = "none")
             )
+            
+            # If no specific gate is provided, use the first actual gate (after root)
             if (is.null(gate_extract)) {
                 # nolint start
                 # [1] "root"
@@ -31,28 +39,34 @@ extract_singlestain_mfi <- function(fcs_dir = "data-raw/s001",
                 gate_extract <- flowWorkspace::gh_get_pop_paths(ff_x_gated)[2]
                 warning("No gate_extract provided, using ", gate_extract)
             }
+            
+            # Extract the gated data
             gated_ff <- flowWorkspace::gs_pop_get_data(ff_x_gated, gate_extract) |>
                 flowWorkspace::realize_view()
             return(gated_ff)
         })
     }
+    
+    # Extract median fluorescence intensities (MFIs) from the loaded FCS files
     relevant_mfis <- sapply(names(loaded_fcs), function(f_x) {
         ff_x <- flowWorkspace::cytoframe_to_flowFrame(loaded_fcs[[f_x]][[1]])
         nonempty_channel <- which(flowCore::markernames(ff_x) != "empty")
+        
+        # Check if there is more than one non-empty channel
         if (length(nonempty_channel) > 1) {
             stop("More than one non-empty channel in ", f_x)
         }
+        
+        # If no non-empty channel, it is the unstained sample
         if (length(nonempty_channel) == 0) {
-            # Then it is the unstained sample --> return the MFI of all channels
             mfis <- apply(flowCore::exprs(ff_x), 2, median)
             mfis_tib <- tibble::tibble(
                 "feature" = names(mfis),
                 "unstained" = mfis,
             )
         } else {
-            # first cluster the relevant channel into TWO populations,
+            # Cluster the relevant channel into two populations and return the median of both
             values_nonempty <- flowCore::exprs(ff_x)[, names(nonempty_channel)]
-            # then return the median of both populations for that channel
             clustering <- stats::kmeans(transform(values_nonempty), centers = 2)
             mfis <- sort(tapply(values_nonempty, clustering$cluster, median))
             mfis_tib <- tibble::tibble(
@@ -65,18 +79,23 @@ extract_singlestain_mfi <- function(fcs_dir = "data-raw/s001",
         return(mfis_tib)
     }, simplify = FALSE)
 
+    # Combine single stainings into a single data frame
     single_stainings <- do.call(rbind, relevant_mfis[!sapply(relevant_mfis, function(x) nrow(x) > 1)])
+    
+    # Extract unstained samples
     relevant_unstained <- relevant_mfis[sapply(relevant_mfis, function(x) nrow(x) > 1)]
+    
+    # Check if there is only one unstained sample
     if (length(relevant_unstained) == 1) {
-        # If there was only one unstained sample, everything was fine, otherwise we just report everything
-        # but that was probably a problem
         names(relevant_unstained) <- "unstained"
     } else {
         warning("More than one unstained sample found, returning each MFI by filename")
     }
 
+    # Join all unstained samples
     all_unstained_joint <- Reduce(dplyr::left_join, relevant_unstained)
 
+    # Return the joined data frame of single stainings and unstained samples
     return(dplyr::left_join(
         single_stainings,
         all_unstained_joint,
