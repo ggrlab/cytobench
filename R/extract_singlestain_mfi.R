6b25b21ad608afa48008deeb37592dc4505f0d9b
gunthergl_r1 <gunthergl@gmx.net>
2024-10-21 17:18:59 +0200

    update extract_singlestain_mfi

--- R/extract_singlestain_mfi.R
+++ R/extract_singlestain_mfi.R
@@ -13,6 +13,32 @@
 #' the gating set is loaded from the file. If a (GatingSet) object is provided, it is used directly.
 #'
 #' @param gate_extract A specific gate to extract from the gating set. Default is NULL.
+#' @param multistaining A logical indicating whether to extract multi-stained samples. Default is FALSE.
+#' Note that if TRUE, the function will return a dataframe as following:
+#'
+#' #' \preformatted{
+#' # A tibble: 10 x 4
+# A tibble: 10 x 7
+#'   feature negative positive unstained sample            negative.multi positive.multi
+#'   <chr>      <dbl>    <dbl>     <dbl> <chr>                      <dbl>          <dbl>
+#' 1 FITC-A      528.    5489.     520.  data-raw/MX.comp~           528.          5489.
+#' 2 PE-A        497.    4224.     477.  data-raw/MX.comp~           497.          4224.
+#' 3 ECD-A       413.    4260.     411.  data-raw/MX.comp~           413.          4260.
+#' 4 PC5.5-A     167.    3306.     145.  data-raw/MX.comp~           167.          3306.
+#' 5 PC7-A       134.    3958.     131.  data-raw/MX.comp~           134.          3958.
+#' 6 APC-A       120.    1896.     106.  data-raw/MX.comp~           120.          1896.
+#' 7 AF700-A     110.    1884.      87.7 data-raw/MX.comp~           110.          1884.
+#' 8 AA750-A     290.    3116.     284.  data-raw/MX.comp~           290.          3116.
+#' 9 PB-A        422.    4434.     388.  data-raw/MX.comp~           422.          4434.
+#' 10 KrO-A       458.    3858.     416.  data-raw/MX.comp~           458.          3858.
+#' }
+#'
+#' If there have been NO single-stained samples (except for the unstained samples),
+#' "negative" and "positive" will be filled with the values from the multi-stained samples.
+#'
+#' @param regex_multistain A regular expression pattern to identify multi-stain FCS files. Default is "(_15-MasterMix)\\.fcs".
+#' @param multistain_columns A character vector specifying the relevant columns for multi-staining. Default is c("FITC-A", "PE-A", "ECD-A", "PC5.5-A", "PC7-A", "APC-A", "AF700-A", "AA750-A", "PB-A", "KrO-A").
+#'
 #' @return A data frame with the extracted MFIs. E.g.:
 #' \preformatted{
 #' # A tibble: 10 x 4
@@ -37,13 +63,102 @@
 #' )
 #' }
 #' @export
+extract_mfi <- function(fcs_dir = "data-raw/s001",
+                        regex_singlestain = "-(CD3-.*)|(none)\\.fcs",
+                        transform = function(x) {
+                            asinh(x / 1e3)
+                        },
+                        multistaining = FALSE,
+                        regex_multistain = "(_15-MasterMix)\\.fcs",
+                        multistain_columns = c(
+                            "FITC-A", "PE-A", "ECD-A", "PC5.5-A", "PC7-A",
+                            "APC-A", "AF700-A", "AA750-A", "PB-A", "KrO-A"
+                        ),
+                        gating_set_file = NULL,
+                        gate_extract = NULL) {
+    loaded_fcs <- load_mfi_files(
+        fcs_dir = fcs_dir,
+        regex_singlestain = regex_singlestain,
+        gating_set_file = gating_set_file,
+        gate_extract = gate_extract
+    )
+    if (!multistaining) {
+        # Extract the single stainings
+        joint_df <- extract_singlestain_mfi_wrapper(loaded_fcs, transform_fun = transform)
+    } else {
+        # If multistaining is enabled, the following extracts the potential UNSTAINED sample
+        joint_df <- extract_singlestain_mfi_wrapper(loaded_fcs, transform_fun = transform, relevant_columns = multistain_columns)
+
+        # Then extract the actually multi-stained sample(s)
+        loaded_fcs_multistain <- load_mfi_files(
+            fcs_dir = fcs_dir,
+            regex_singlestain = regex_multistain,
+            gating_set_file = gating_set_file,
+            gate_extract = gate_extract
+        )
+        relevant_mfis_multi <- tryCatch(
+            {
+                extract_relevant_mfis_multistain(
+                    loaded_fcs_multistain,
+                    transform_fun = transform,
+                    relevant_columns = multistain_columns
+                )
+            },
+            error = function(e) {
+                list(tibble::tibble(
+                    "sample" = NA,
+                    "feature" = NA,
+                    "negative" = NA,
+                    "positive" = NA,
+                    "unstained" = NA
+                ))
+            }
+        )
+        #  Merge single and multi stainings
+        joint_df <- dplyr::left_join(
+            joint_df,
+            relevant_mfis_multi,
+            by = "feature",
+            suffix = c("", ".multi")
+        )
+        if (all(is.na(joint_df[["negative"]]))) {
+            joint_df[["negative"]] <- joint_df[["negative.multi"]]
+        }
+        if (all(is.na(joint_df[["positive"]]))) {
+            joint_df[["positive"]] <- joint_df[["positive.multi"]]
+        }
+    }
+
+    # Unname each column
+    return(dplyr::mutate(joint_df, dplyr::across(tidyr::everything(), ~ unname(.))))
+}
+
+#' Extract Single Stain Median Fluorescence Intensity (MFI)
+#' @inheritParams extract_mfi
+#' @export
 extract_singlestain_mfi <- function(fcs_dir = "data-raw/s001",
-                                    regex_singlestain = "-(CD3-.*)|(none)\\.fcs",
-                                    transform = function(x) {
-                                        asinh(x / 1e3)
-                                    },
-                                    gating_set_file = NULL,
-                                    gate_extract = NULL) {
+                                   regex_singlestain = "-(CD3-.*)|(none)\\.fcs",
+                                   transform = function(x) {
+                                       asinh(x / 1e3)
+                                   },
+                                   gating_set_file = NULL,
+                                   gate_extract = NULL) {
+    .Deprecated("extract_mfi")
+    extract_mfi(
+        fcs_dir = fcs_dir,
+        regex_singlestain = regex_singlestain,
+        transform = transform,
+        gating_set_file = gating_set_file,
+        gate_extract = gate_extract,
+        multistaining = FALSE
+    )
+}
+
+load_mfi_files <- function(fcs_dir = "data-raw/s001",
+                           regex_singlestain = "-(CD3-.*)|(none)\\.fcs",
+                           gating_set_file = NULL,
+                           gate_extract = NULL) {
+
     # List all files in the specified directory that match the given regex pattern
     dir_files <- list.files(fcs_dir, full.names = TRUE, pattern = regex_singlestain)
 
@@ -86,14 +201,108 @@ extract_singlestain_mfi <- function(fcs_dir = "data-raw/s001",
         })
     }
 
+    return(loaded_fcs)
+}
+
+#' Extract Single Stain Median Fluorescence Intensity (MFI)
+#' This function extracts the median fluorescence intensity (MFI) from single-stain FCS files.
+#'
+#' @param loaded_fcs A list of cytosets containing the loaded FCS files.
+#' @param transform_fun A function to transform the fluorescence values. Default is `function(x) { asinh(x / 1e3) }`.
+#' The reported MFIs are calculated as the median of the UNtransformed values. Transformation is
+#' only used to cluster the negative and positive populations.
+#' @return A data frame with the extracted MFIs. E.g.:
+#' \preformatted{
+#' # A tibble: 10 x 4
+#'    feature negative positive unstained
+#'    <chr>      <dbl>    <dbl>     <dbl>
+#'  1 FITC-A      530.   58567.     576.
+#'  2 PE-A        526.  149506.     511.
+#'  3 ECD-A       453.  107584.     454.
+#'  4 PC5.5-A     233.  242867.     166.
+#'  5 PC7-A       176.  195307.     138.
+#'  6 APC-A       136.  105036.     122.
+#'  7 AF700-A     131.   28408.      97.2
+#'  8 AA750-A     324.   75247.     296.
+#'  9 PB-A        469.   13426.     442.
+#' 10 KrO-A       442.    4473.     444.
+#' }
+extract_singlestain_mfi_wrapper <- function(loaded_fcs,
+                                            transform_fun = function(x) {
+                                                asinh(x / 1e3)
+                                            },
+                                            relevant_columns,
+                                            ...) {
     # Extract median fluorescence intensities (MFIs) from the loaded FCS files
-    relevant_mfis <- sapply(names(loaded_fcs), function(f_x) {
-        ff_x <- flowWorkspace::cytoframe_to_flowFrame(loaded_fcs[[f_x]][[1]])
+    relevant_mfis_single <- tryCatch(
+        {
+            extract_relevant_mfis_singlestain(loaded_fcs, transform_fun = transform, ...)
+        },
+        error = function(e) {
+            list(tibble::tibble(
+                "feature" = NA,
+                "negative" = NA,
+                "positive" = NA
+            ))
+        }
+    )
+    # Combine single stainings into a single data frame
+    single_stainings <- do.call(rbind, relevant_mfis_single[!sapply(relevant_mfis_single, function(x) nrow(x) > 1)])
+    if (!missing(relevant_columns)) {
+        for (relevant_x in relevant_columns) {
+            if (!relevant_x %in% single_stainings$feature) {
+                single_stainings <- rbind(single_stainings, tibble::tibble(
+                    "feature" = relevant_x,
+                    "negative" = NA,
+                    "positive" = NA
+                ))
+            }
+        }
+    }
+    single_stainings <- single_stainings[!is.na(single_stainings[["feature"]]), ]
+
+    # Extract unstained samples
+    relevant_unstained <- relevant_mfis_single[sapply(relevant_mfis_single, function(x) nrow(x) > 1)]
+
+    # Check if there is only one unstained sample
+    if (length(relevant_unstained) == 1) {
+        names(relevant_unstained) <- "unstained"
+    } else {
+        warning("More than one unstained sample found, returning each MFI by filename")
+    }
+
+    # Join all unstained samples
+    all_unstained_joint <- Reduce(dplyr::left_join, relevant_unstained)
+    if (!is.null(all_unstained_joint)) {
+        # Return the joined data frame of single stainings and unstained samples
+        if (any(is.na(single_stainings[["feature"]]))) {
+            stop("There are NA values in the feature column of single_stainings, joining with unstained will fail.")
+        }
+        joint_df <- dplyr::left_join(
+            single_stainings,
+            all_unstained_joint,
+            by = "feature"
+        )
+    }
+    return(joint_df)
+}
+extract_relevant_mfis_singlestain <- function(loaded_fcs_singlestain,
+                                              transform_fun = function(x) {
+                                                  x
+                                              },
+                                              seed = 42) {
+    sapply(names(loaded_fcs_singlestain), function(f_x) {
+        ff_x <- flowWorkspace::cytoframe_to_flowFrame(loaded_fcs_singlestain[[f_x]][[1]])
         nonempty_channel <- which(flowCore::markernames(ff_x) != "empty")
 
         # Check if there is more than one non-empty channel
         if (length(nonempty_channel) > 1) {
-            stop("More than one non-empty channel in ", f_x)
+            warning("More than one non-empty channel in ", f_x)
+            return(tibble::tibble(
+                "feature" = NA,
+                "negative" = NA,
+                "positive" = NA
+            ))
         }
 
         # If no non-empty channel, it is the unstained sample
@@ -106,7 +315,8 @@ extract_singlestain_mfi <- function(fcs_dir = "data-raw/s001",
         } else {
             # Cluster the relevant channel into two populations and return the median of both
             values_nonempty <- flowCore::exprs(ff_x)[, names(nonempty_channel)]
-            clustering <- stats::kmeans(transform(values_nonempty), centers = 2)
+            set.seed(seed)
+            clustering <- stats::kmeans(transform_fun(values_nonempty), centers = 2)
             mfis <- sort(tapply(values_nonempty, clustering$cluster, median))
             mfis_tib <- tibble::tibble(
                 "feature" = names(nonempty_channel),
@@ -117,29 +327,32 @@ extract_singlestain_mfi <- function(fcs_dir = "data-raw/s001",
 
         return(mfis_tib)
     }, simplify = FALSE)
+}
 
-    # Combine single stainings into a single data frame
-    single_stainings <- do.call(rbind, relevant_mfis[!sapply(relevant_mfis, function(x) nrow(x) > 1)])
-
-    # Extract unstained samples
-    relevant_unstained <- relevant_mfis[sapply(relevant_mfis, function(x) nrow(x) > 1)]
-
-    # Check if there is only one unstained sample
-    if (length(relevant_unstained) == 1) {
-        names(relevant_unstained) <- "unstained"
-    } else {
-        warning("More than one unstained sample found, returning each MFI by filename")
+extract_relevant_mfis_multistain <- function(loaded_fcs_multistain,
+                                             transform_fun = function(x) {
+                                                 asinh(x / 1e3)
+                                             },
+                                             relevant_columns,
+                                             seed = 42) {
+    if (length(loaded_fcs_multistain) != 1) {
+        warning("More than one multistain sample found, returning the MFI of each multistain_column by filename. This is NOT intended!")
     }
-
-    # Join all unstained samples
-    all_unstained_joint <- Reduce(dplyr::left_join, relevant_unstained)
-
-    # Return the joined data frame of single stainings and unstained samples
-    joint_df <- dplyr::left_join(
-        single_stainings,
-        all_unstained_joint,
-        by = "feature"
-    )
-    # Unname each column
-    return(dplyr::mutate(joint_df, dplyr::across(tidyr::everything(), ~unname(.))) )
+    multistain_mfis <- lapply(loaded_fcs_multistain, function(ff_x) {
+        values_relevantcols <- flowCore::exprs(flowWorkspace::cytoframe_to_flowFrame(ff_x[, relevant_columns][[1]]))
+        mfis <- apply(values_relevantcols, 2, function(col_x) {
+            set.seed(seed)
+            clustering <- stats::kmeans(transform_fun(col_x), centers = 2)
+            mfis <- sort(tapply(col_x, clustering$cluster, median))
+            mfis_tib <- tibble::tibble(
+                # "feature" = names(nonempty_channel),
+                "negative" = mfis[1],
+                "positive" = mfis[2]
+            )
+        })
+        mfis_tib <- data.table::rbindlist(mfis, idcol = "feature")
+    }) |>
+        data.table::rbindlist(idcol = "sample") |>
+        tibble::as_tibble()
+    return(multistain_mfis)
 }
--- tests/testthat/test-extract_singlestain_mfi.R
+++ tests/testthat/test-extract_singlestain_mfi.R
@@ -5,25 +5,25 @@ test_that("Extract single negative MFI", {
     )
 
     # # A tibble: 10 × 4
-    #    feature negative positive unstained
-    #    <chr>      <dbl>    <dbl>     <dbl>
-    #  1 FITC-A      530.   58567.     576.
-    #  2 PE-A        526.  149506.     511.
-    #  3 ECD-A       453.  107584.     454.
-    #  4 PC5.5-A     233.  242867.     166.
-    #  5 PC7-A       176.  195307.     138.
-    #  6 APC-A       136.  105036.     122.
-    #  7 AF700-A     131.   28408.      97.2
-    #  8 AA750-A     324.   75247.     296.
-    #  9 PB-A        469.   13426.     442.
-    # 10 KrO-A       442.    4473.     444.
+    # feature negative positive unstained
+    # <chr>      <dbl>    <dbl>     <dbl>
+    # 1 FITC-A      542.   60575.     576. 
+    # 2 PE-A        551.  162441.     511. 
+    # 3 ECD-A       468.  112634.     454. 
+    # 4 PC5.5-A     242.  251954.     166. 
+    # 5 PC7-A       186.  204156      138. 
+    # 6 APC-A       142.  108628.     122. 
+    # 7 AF700-A     135.   29050.      97.2
+    # 8 AA750-A     333.   77465.     296. 
+    # 9 PB-A        478.   14059.     442. 
+    # 10 KrO-A       449.    4665.     444.
     testthat::expect_equal(
         extracted_mfis,
         tibble::tibble(
-            feature = c("FITC-A", "PE-A", "ECD-A", "PC5.5-A", "PC7-A", "APC-A", "AF700-A", "AA750-A", "PB-A", "KrO-A"),
-            negative = c(530, 526, 453, 233, 176, 136, 131, 324, 469, 442),
-            positive = c(58567, 149506, 107584, 242867, 195307, 105036, 28408, 75247, 13426, 4473),
-            unstained = c(576, 511, 454, 166, 138, 122, 97.2, 296, 442, 444)
+            "feature" = c("FITC-A", "PE-A", "ECD-A", "PC5.5-A", "PC7-A", "APC-A", "AF700-A", "AA750-A", "PB-A", "KrO-A"),
+            "negative" = c(542, 551, 468, 242, 186, 142, 135, 333, 478, 449),
+            "positive" = c(60575, 162441, 112634, 251954, 204156, 108628, 29050, 77465, 14059, 4665),
+            "unstained" = c(576, 511, 454, 166, 138, 122, 97.2, 296, 442, 444)
         ),
         tolerance = .1
     )
