0666d76b6333f32b175891125b82cfbed4ff60f1
gunthergl_r1 <gunthergl@gmx.net>
2025-08-05 10:17:51 +0200

    document tempdir_time

--- R/tempdir_time.R
+++ R/tempdir_time.R
@@ -1,9 +1,19 @@
-#' Create a temporary directory with a timestamp.
+#' Create Timestamped Temporary Directory
+#'
+#' Creates a new temporary directory by appending a timestamp to the default `tempdir()`.
+#' This is useful for generating isolated working directories in pipelines or scripts.
+#'
+#' @return A character string containing the full path to the newly created timestamped directory.
+#' The directory is created immediately on disk.
+#'
+#' @examples
+#' tmp <- tempdir_time()
+#' list.files(tmp) # should be empty
 #'
 #' @export
 tempdir_time <- function() {
     tmpdir <- tempdir()
     tmpdir_time <- paste0(tmpdir, "_", format(Sys.time(), "%F_%H_%M_%OS2"))
-    dir.create(tmpdir_time)
+    dir.create(tmpdir_time, recursive = TRUE)
     return(tmpdir_time)
 }
