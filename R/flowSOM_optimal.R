4435c71070b784e4eb70de712fcd7b71c748e338
gunthergl_r1 <gunthergl@gmx.net>
2025-08-05 08:13:57 +0200

    document flowSOM_optimal

--- R/flowSOM_optimal.R
+++ R/flowSOM_optimal.R
@@ -1,12 +1,14 @@
-#' @title flowSOM clustering with optimal number of clusters
+#' @title FlowSOM Clustering with Optimal Number of MetaClusters
+#'
 #' @description
-#' Clusters the cells using the FlowSOM algorithm with the optimal number of clusters (determined by ConsensusClusterPlus), by setting maxMeta.
-#' @param fs_train
-#' The training set of the cytometry data.
-#' @param colsToUse
-#' The columns to use for clustering.
-#' @param transformList
-#' The list of transformations to apply to the data. E.g.
+#' Runs the FlowSOM algorithm on a cytometry dataset and determines an optimal number
+#' of meta-clusters using `ConsensusClusterPlus`. Automatically generates and optionally
+#' saves plots and the clustering result. Can also return per-cell cluster predictions.
+#'
+#' @param fs_train A `flowSet` or `cytoset` representing the cytometry training data.
+#' @param colsToUse Character vector. Column names (channels/markers) to include in clustering.
+#' @param transformList A `transformList` (typically from `flowCore::transformList`) defining the transformations to apply.
+#'        If `transform = TRUE`, this will be applied during clustering.E.g.
 #'
 #' fc_transforms <- sapply(colsToUse, function(cn_x) {
 #'     flowCore::arcsinhTransform(
@@ -16,17 +18,19 @@
 #'     )
 #' }, simplify = FALSE)
 #' fc_transformlist_asinh <- flowCore::transformList(names(fc_transforms), fc_transforms)
-#' @param outdir
-#' The directory to save the FlowSOM results.
-#' @param seed
-#' The seed for the random number generator.
-#' @param scale
-#' Whether to scale the data.
-#' @param transform
-#' Whether to transform the data using transformList
-#' @param maxMeta
-#' The maximal number of metaclusters. If NULL, the number of clusters is FIXED by nClus.
-#' @param ... Further parameters to FlowSOM
+#' @param outdir Optional. Directory path to save the FlowSOM result object and associated PDF plots.
+#' @param seed Integer. Random seed for reproducibility. Default is `3711283`.
+#' @param scale Logical. Whether to scale data (default `TRUE`). Scaling is applied prior to SOM training.
+#' @param transform Logical. Whether to apply `transformList` before clustering (default `TRUE`).
+#' @param maxMeta Integer. Maximum number of meta-clusters to consider. If `NULL`, defaults to a FIXED number via `nClus` argument.
+#' @param ... Additional arguments passed to `FlowSOM::FlowSOM()`.
+#'
+#' @return An object of class `"flowSOM_optimal"`, which includes:
+#' \describe{
+#'   \item{`fs_res_train`}{The FlowSOM result object with meta-clustering annotations.}
+#'   \item{...}{Additional elements returned by `flowSOM_predict()`, including predicted cluster and meta-cluster labels.}
+#' }
+#'
 #' @export
 flowSOM_optimal <- function(fs_train,
                             colsToUse,
@@ -35,16 +39,20 @@ flowSOM_optimal <- function(fs_train,
                             outdir = NULL,
                             seed = 3711283,
                             scale = TRUE,
-                            maxMeta = 50, # Number of MetaClusters, automatically determined by ConsensusClusterPlus
+                            maxMeta = 50, # Max number of metaClusters
                             ...) {
+    # Warn if very few meta-clusters are requested
     if (is.null(maxMeta) || maxMeta < 10) {
         warning("Are you sure you want to set maxMeta to ", maxMeta, "? This is the maximal number of possible metaclusters.")
     }
+
+    # If user provides nClus directly, override maxMeta and show warning
     if ("nClus" %in% names(list(...))) {
         warning("By using nClus, I am ignoring the parameter maxMeta!")
         maxMeta <- NULL
     }
-    ### 1. Run FlowSOM
+
+    # 1. Run the FlowSOM clustering algorithm
     fs_res_train <- FlowSOM::FlowSOM(
         input = fs_train,
         transform = transform,
@@ -56,12 +64,19 @@ flowSOM_optimal <- function(fs_train,
         ...
     )
     fs_res_train[["seed"]] <- seed
-    # The following creates the metacluster dataframe for ALL metaclusterings
-    #  fs_res_train$ConsensusClusterPlus_MAP
-    fs_res_train_allmeta <- flowSOM_newMetacluster(fs_res_train, n_metacluster = max(maxMeta, list(...)[["nClus"]]), update_flowsom = FALSE)
+
+    # 2. Precompute all meta-clusterings for ConsensusClusterPlus
+    # With this, we can later very quickly get all meta-clusterings up until n_meta using
+    # flowSOM_newMetacluster(fs_res_train_allmeta, n_metacluster = XXXX)
+    n_meta <- max(maxMeta, list(...)[["nClus"]])
+    fs_res_train_allmeta <- flowSOM_newMetacluster(
+        fs_res_train,
+        n_metacluster = n_meta,
+        update_flowsom = FALSE
+    )
     fs_res_train <- fs_res_train_allmeta[["flowsom_newdata"]]
 
-    ### 2. Plot and save FlowSOM results into outdir
+    # 3. Save result and FlowSOM plots if requested
     if (!is.null(outdir)) {
         dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
         qs::qsave(fs_res_train, file.path(outdir, "r1-FlowSOM_result_train.qs"))
@@ -71,13 +86,9 @@ flowSOM_optimal <- function(fs_train,
             fs_res_train,
             backgroundValues = fs_res_train$metaclustering
         )
-        FlowSOM::PlotNumbers(
-            fs_res_train
-        )
-        FlowSOM::PlotNumbers(
-            fs_res_train,
-            level = "metaclusters"
-        )
+        FlowSOM::PlotNumbers(fs_res_train)
+        FlowSOM::PlotNumbers(fs_res_train, level = "metaclusters")
+
         for (marker_x in colsToUse) {
             print(
                 FlowSOM::PlotMarker(
@@ -90,14 +101,16 @@ flowSOM_optimal <- function(fs_train,
         dev.off()
     }
 
+    # 4. Predict the per-cell cluster and metacluster assignment
     predicted_fs <- flowSOM_predict(
         flowsom_result = fs_res_train,
         flowset = fs_train
     )
-    res <-c(
-        list(
-            fs_res_train = fs_res_train
-        ), predicted_fs
+
+    # 5. Combine and return results
+    res <- c(
+        list(fs_res_train = fs_res_train),
+        predicted_fs
     )
     class(res) <- c("flowSOM_optimal")
     return(res)
