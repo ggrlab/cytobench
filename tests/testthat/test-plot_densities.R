00b5e0454bd4e139d613e8a100e8015b1d785c59
gunthergl_r1 <gunthergl@gmx.net>
2026-03-03 11:01:56 +0100

    updated densityplots: expected warnings

--- tests/testthat/test-plot_densities.R
+++ tests/testthat/test-plot_densities.R
@@ -3,9 +3,13 @@ test_that("Plotting densities", {
     fs_dt <- flowCore::fsApply(fs, function(x) data.table::as.data.table(flowCore::exprs(x))) |>
         data.table::rbindlist(idcol = "File")
     testthat::expect_error(
-        plot_densities(fs_dt),
+        testthat::expect_warning(
+            plot_densities(fs_dt),
+            "'measure.vars' \\[File, CD4, CD8\\] are not all of the same type. By order of hierarchy, the molten data value column will be of type 'character'"
+        ),
         regexp = "Expression values after melting cannot be character. Did you set groupings and markers correctly?"
     )
+    browser()
     plot_densities(fs_dt, markers = c("CD4", "CD8"))
     plot_densities(fs_dt, column_color = "File")
 
@@ -15,10 +19,16 @@ test_that("Plotting densities", {
         groupings = "File"
     )
     plot_densities(densities, column_color = "File")
-    plot_densities(densities)
+    testthat::expect_warning(
+        plot_densities(densities),
+        "The following columns in densities_dt are not used for plotting and may lead to wrong/weird plots: File"
+    )
 
     testthat::expect_error(
-        densities <- calc_densities(dt = fs_dt, groupings = NULL),
+        testthat::expect_warning(
+            densities <- calc_densities(dt = fs_dt, groupings = NULL),
+            "'measure.vars' \\[File, CD4, CD8\\] are not all of the same type. By order of hierarchy, the molten data value column will be of type 'character'"
+        ),
         regexp = "Expression values after melting cannot be character. Did you set groupings and markers correctly?"
     )
     densities <- calc_densities(
